37bd5ff8f6adbe6174305f05de4f408e4cd11513
Jan Wesseling <jan@wesselings.de>
2025-01-19 11:00:06 +0100

    Increment version to 2 in config flow for Windhager heater integration
    - Updated the VERSION constant in config_flow.py to reflect the new version.

--- custom_components/windhager/config_flow.py
+++ custom_components/windhager/config_flow.py
@@ -76,7 +76,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for heater."""
 
-    VERSION = 1
+    VERSION = 2
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
